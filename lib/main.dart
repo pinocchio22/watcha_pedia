71cd9e0812f6881ced361cdc06ba54f1908b6708
p2glet <vviian.2@gmail.com>
2023-06-21 16:23:15 +0900

    LikedBookPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import 'book.dart';
 import 'book_service.dart';
 
 void main() {
@@ -101,11 +102,63 @@ class SearchPage extends StatelessWidget {
               ),
             ),
           ),
-          body: Center(
-            child: Text("검색"),
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12),
+            child: ListView.separated(
+              itemCount: bookService.bookList.length,
+              separatorBuilder: (context, index) {
+                return Divider();
+              },
+              itemBuilder: (context, index) {
+                if (bookService.bookList.isEmpty) return SizedBox();
+                Book book = bookService.bookList.elementAt(index);
+                return BookTile(book: book);
+              },
+            ),
           ),
         );
       },
+    );
+  }
+}
+
+class BookTile extends StatelessWidget {
+  const BookTile({
+    super.key,
+    required this.book,
+  });
+
+  final Book book;
+
+  @override
+  Widget build(BuildContext context) {
+    BookService bookService = context.read<BookService>();
+
+    return ListTile(
+      onTap: () {},
+      leading: Image.network(
+        book.thumbnail,
+        fit: BoxFit.fitHeight,
+      ),
+      title: Text(
+        book.title,
+        style: TextStyle(fontSize: 16),
+      ),
+      subtitle: Text(
+        book.subtitle,
+        style: TextStyle(color: Colors.grey),
+      ),
+      trailing: IconButton(
+        onPressed: () {
+          bookService.toggleLikeBook(book: book);
+        },
+        icon: bookService.likedBookList.map((book) => book.id).contains(book.id)
+            ? Icon(
+                Icons.star,
+                color: Colors.amber,
+              )
+            : Icon(Icons.star_border),
+      ),
     );
   }
 }
